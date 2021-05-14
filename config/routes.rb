466f120c83a2a79d0a10ebaea93a70575b57b954
nicolasdubet <nicolas.dubet@orange.fr>
2021-05-14 15:40:28 +0200

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
